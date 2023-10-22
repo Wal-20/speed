e7b769f47f48d648eddc28dc126f0c980585f894
Waleed <waleedabdulhadi4@gmail.com>
2023-10-22 21:32:19 +0300

    changed button background colors

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -2,20 +2,19 @@ import {React} from 'react'
 import { getRandomInt } from '../utils';
 import { validateChoice } from '../utils';
 import { words } from '../utils';
-import { updateHighscore } from '../utils';
 import { getRandomDistinctElements } from '../utils';
 import CountdownTimer from './CountdownTimer';
 import users from '../users.json'
 
 
-const Game = ({score,setPage,setScore,word_count,seconds}) => {
+const Game = ({score,page,setPage,setScore,word_count,seconds}) => {
   const choices = getRandomDistinctElements(words,word_count);
   const word = choices[getRandomInt(word_count)];
 
     const buttonIndices = Array.from({ length: word_count }, (_, index) => index);
 
     return (
-      <>
+      <div>
           <h1 id='Word'>{word}</h1> 
         {/** the string in this h1 or the word choice buttons cannot have a space or a line break because the choice is compared to it, so any added special characters will ruin the comparison */}
         {/** for example: 'open' != 'open\n' != 'open ' */}
@@ -24,14 +23,12 @@ const Game = ({score,setPage,setScore,word_count,seconds}) => {
 
         {buttonIndices.map((index) => (
           <>
-            <button id={`b${index + 1}`} onClick={() => {
-              validateChoice(`b${index + 1}`, setScore, setPage);
-              }}
-              type="button" className="btn btn-primary">
+            <button id={`b${index + 1}`} className={ page === 'game easy' ? 'easy-buttons' : page === 'game medium' ? 'medium-buttons' : 'hard-buttons'}
+             onClick={() => { validateChoice(`b${index + 1}`, setScore, setPage) }} type="button">
 
               {choices[index]}
-
             </button>
+
             {(index + 1) % 4 === 0 && <br/>}
           </>
           
@@ -40,10 +37,10 @@ const Game = ({score,setPage,setScore,word_count,seconds}) => {
 
 
           <CountdownTimer seconds={seconds} onTimeout={setPage}/>
-       
+            
           <h2> Score: {score} </h2>
 
-      </>
+      </div>
     )
   }
 
